8e9083dd6337bf34b85ccd6d03aae021b6b23980
Anna Savina <anna.s.savina@gmail.com>
2021-07-17 19:24:08 +0300

    Refactoring Query

--- src/Boxyz.Api.GraphQL/Queries/BoxContextQuery.cs
+++ src/Boxyz.Api.GraphQL/Queries/BoxContextQuery.cs
@@ -20,59 +20,10 @@ namespace Boxyz.Api.GraphQL
         public BoxContextQuery(IBoxServiceContext srvContext, IBoxService boxService, IHttpContextAccessor httpContextAccessor)
         {
 #if DEBUG
-            AddRawQueries(httpContextAccessor);
+            AddRawEntitiesQueries(httpContextAccessor);
 #endif
-
-            FieldAsync<ShapeBoardType>(
-                "hierBoard",
-                arguments: new QueryArguments(
-                    new QueryArgument<NonNullGraphType<BigIntGraphType>> { Name = "id" }
-                ),
-                resolve: async context => await srvContext.ShapeBoardService.GetOne(context.GetArgument<long>("id"))
-            );
-
-            FieldAsync<ShapeType>(
-                "hierShape",
-                arguments: new QueryArguments(
-                    new QueryArgument<NonNullGraphType<BigIntGraphType>> { Name = "id" }
-                ),
-                resolve: async context => await srvContext.ShapeService.GetOne(context.GetArgument<long>("id"))
-            );
-
-            FieldAsync<BoxType>(
-                "hierBox",
-                arguments: new QueryArguments(
-                    new QueryArgument<NonNullGraphType<BigIntGraphType>> { Name = "id" }
-                ),
-                resolve: async context => await srvContext.BoxService.GetOne(context.GetArgument<long>("id"))
-            );
-
-            FieldAsync<ShapeBoardFlatType>(
-                "flatBoard",
-                arguments: new QueryArguments(
-                    new QueryArgument<NonNullGraphType<BigIntGraphType>> { Name = "id" },
-                    new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "culture" }
-                ),
-                resolve: async context => await srvContext.ShapeBoardService.GetFlat(context.GetArgument<long>("id"), context.GetArgument<string>("culture"))
-            );
-
-            FieldAsync<ShapeFlatType>(
-                "flatShape",
-                arguments: new QueryArguments(
-                    new QueryArgument<NonNullGraphType<BigIntGraphType>> { Name = "id" },
-                    new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "culture" }
-                ),
-                resolve: async context => await srvContext.ShapeService.GetFlat(context.GetArgument<long>("id"), context.GetArgument<string>("culture"))
-            );
-
-            FieldAsync<BoxFlatType>(
-                "flatBox",
-                arguments: new QueryArguments(
-                    new QueryArgument<NonNullGraphType<BigIntGraphType>> { Name = "id" },
-                    new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "culture" }
-                ),
-                resolve: async context => await srvContext.BoxService.GetFlat(context.GetArgument<long>("id"), context.GetArgument<string>("culture"))
-            );
+            AddFlatModelQueries(httpContextAccessor);
+            AddHierarchicModelQueries(httpContextAccessor);
 
             FieldAsync<StringGraphType>(
                 "boxObject",
@@ -86,6 +37,45 @@ namespace Boxyz.Api.GraphQL
                     var json = JsonSerializer.Serialize(obj);
                     return json;
                 }              
+            );   
+        }
+
+        private void AddHierarchicModelQueries(IHttpContextAccessor httpContextAccessor)
+        {
+            FieldAsync<ShapeBoardType>(
+                "hierBoard",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<BigIntGraphType>> { Name = "id" }
+                ),
+                resolve: async context =>
+                {
+                    using var scope = httpContextAccessor.CreateScope();
+                    return await scope.GetService<IShapeBoardDalService>().GetOne(context.GetArgument<long>("id"));
+                }
+            );
+
+            FieldAsync<ShapeType>(
+                "hierShape",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<BigIntGraphType>> { Name = "id" }
+                ),
+                resolve: async context =>
+                {
+                    using var scope = httpContextAccessor.CreateScope();
+                    return await scope.GetService<IShapeDalService>().GetOne(context.GetArgument<long>("id"));
+                }
+            );
+
+            FieldAsync<BoxType>(
+                "hierBox",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<BigIntGraphType>> { Name = "id" }
+                ),
+                resolve: async context =>
+                {
+                    using var scope = httpContextAccessor.CreateScope();
+                    return await scope.GetService<IBoxDalService>().GetOne(context.GetArgument<long>("id"));
+                }
             );
 
             FieldAsync<ListGraphType<ShapeBoardType>>(
@@ -94,7 +84,53 @@ namespace Boxyz.Api.GraphQL
                     new QueryArgument<NonNullGraphType<IntGraphType>> { Name = "page" },
                     new QueryArgument<NonNullGraphType<IntGraphType>> { Name = "count" }
                 ),
-                resolve: async context => await srvContext.ShapeBoardService.GetAll(context.GetArgument<int>("page"), context.GetArgument<int>("count"))
+                resolve: async context =>
+                {
+                    using var scope = httpContextAccessor.CreateScope();
+                    return await scope.GetService<IShapeBoardDalService>().GetAll(context.GetArgument<int>("page"), context.GetArgument<int>("count"));
+                }
+            );
+        }
+
+        private void AddFlatModelQueries(IHttpContextAccessor httpContextAccessor)
+        {
+            FieldAsync<ShapeBoardFlatType>(
+                "flatBoard",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<BigIntGraphType>> { Name = "id" },
+                    new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "culture" }
+                ),
+                resolve: async context =>
+                {
+                    using var scope = httpContextAccessor.CreateScope();
+                    return await scope.GetService<IShapeBoardDalService>().GetFlat(context.GetArgument<long>("id"), context.GetArgument<string>("culture"));
+                }
+            );
+
+            FieldAsync<ShapeFlatType>(
+                "flatShape",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<BigIntGraphType>> { Name = "id" },
+                    new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "culture" }
+                ),
+                resolve: async context =>
+                {
+                    using var scope = httpContextAccessor.CreateScope();
+                    return await scope.GetService<IShapeDalService>().GetFlat(context.GetArgument<long>("id"), context.GetArgument<string>("culture"));
+                }
+            );
+
+            FieldAsync<BoxFlatType>(
+                "flatBox",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<BigIntGraphType>> { Name = "id" },
+                    new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "culture" }
+                ),
+                resolve: async context =>
+                {
+                    using var scope = httpContextAccessor.CreateScope();
+                    return await scope.GetService<IBoxDalService>().GetFlat(context.GetArgument<long>("id"), context.GetArgument<string>("culture"));
+                }
             );
 
             FieldAsync<ListGraphType<ShapeBoardFlatType>>(
@@ -104,16 +140,19 @@ namespace Boxyz.Api.GraphQL
                     new QueryArgument<NonNullGraphType<IntGraphType>> { Name = "count" },
                     new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "culture" }
                 ),
-                resolve: async context => await srvContext.ShapeBoardService.GetAllFlat(context.GetArgument<int>("page"), 
-                    context.GetArgument<int>("count"), context.GetArgument<string>("culture"))
+                resolve: async context =>
+                {
+                    using var scope = httpContextAccessor.CreateScope();
+                    return await scope.GetService<IShapeBoardDalService>().GetAllFlat(context.GetArgument<int>("page"),
+                        context.GetArgument<int>("count"), context.GetArgument<string>("culture"));
+                }
             );
         }
-
 
         /// <summary> 
         /// Very experimental
         /// </summary>
-        private void AddRawQueries(IHttpContextAccessor httpContextAccessor)
+        private void AddRawEntitiesQueries(IHttpContextAccessor httpContextAccessor)
         {
             // Something is broken here
             // An attempt was made to lazy-load navigation 'Cultures.ShapeBoardProxy' after the associated DbContext was disposed.
