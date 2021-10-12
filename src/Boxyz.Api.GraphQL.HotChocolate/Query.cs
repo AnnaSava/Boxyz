89f3bb015f79272fcb9608bed6967cc87e1d9bcc
Anna Savina <anna.s.savina@gmail.com>
2021-10-12 23:27:22 +0300

    Project settings

--- src/Boxyz.Api.GraphQL.HotChocolate/Query.cs
+++ src/Boxyz.Api.GraphQL.HotChocolate/Query.cs
@@ -1,11 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Boxyz.Proto.Api.GraphQL.HotChocolate
+﻿namespace Boxyz.Proto.Api.GraphQL.HotChocolate
 {
     public class Query
     {
+        public Person GetPerson() => new Person("Luke Skywalker");
+    }
+
+    public class Person
+    {
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
     }
 }
--- src/Boxyz.Api.GraphQL.HotChocolate/Startup.cs
+++ src/Boxyz.Api.GraphQL.HotChocolate/Startup.cs
@@ -1,16 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Boxyz.Proto.Api.GraphQL.HotChocolate
 {
@@ -34,12 +26,17 @@ namespace Boxyz.Proto.Api.GraphQL.HotChocolate
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app
-                .UseRouting()
-                .UseEndpoints(endpoints =>
-                {
-                    endpoints.MapGraphQL();
-                });
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGraphQL();
+            });
         }
     }
 }
